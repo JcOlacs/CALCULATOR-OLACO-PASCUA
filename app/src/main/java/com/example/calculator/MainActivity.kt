fa2d82190e43f5f44a0e993806c617945b1c3019
JcOlacs <olacojohnclifford@gmail.com>
2024-08-31 20:13:03 +0800

    Calculator/Olaco-Pascua

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,233 @@
+package com.example.calculator
+
+import android.graphics.Path.Op
+import android.health.connect.datatypes.units.Percentage
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.calculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    private var inputValue1: Double? = 0.0
+    private var inputValue2: Double? = null
+    private var result: Double? = null
+    private var currentOperator: Operator? = null
+    private val equation: StringBuilder = StringBuilder().append(ZERO)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun setListeners() {
+        for (button in getNumericButtons()) {
+            button.setOnClickListener { onNumberClicked(button.text.toString()) }
+        }
+        with(binding) {
+            buttonZero.setOnClickListener { onZeroClicked() }
+            buttonDoubleZero.setOnClickListener { onDoubleZeroClicked() }
+            buttonDecimalPoint.setOnClickListener { onDecimalPointClicked() }
+            buttonAddition.setOnClickListener { onOperatorClicked(Operator.ADDITION) }
+            buttonSubtraction.setOnClickListener { onOperatorClicked(Operator.SUBTRACTION) }
+            buttonMultiplication.setOnClickListener { onOperatorClicked(Operator.MULTIPLICATION) }
+            buttonDivision.setOnClickListener { onOperatorClicked(Operator.DIVISION) }
+            buttonEqual.setOnClickListener { onEqualClicked() }
+            buttonAllClearBtn.setOnClickListener { onAllClearClicked() }
+            buttonPlusMinusBtn.setOnClickListener { onPlusMinusClicked() }
+            buttonPercentage.setOnClickListener { onPercentageClicked() }
+        }
+    }
+
+    private fun onPercentageClicked() {
+        if (inputValue2 == null) {
+            val percentage = getInputValue1() / 100
+            inputValue1 = percentage
+            equation.clear().append(percentage)
+            updateInputOnDisplay()
+        } else {
+            val percentageOfValue1 = (getInputValue1() * getInputValue1()) / 100
+            val percentageOfValue2 = getInputValue2() / 100
+            result = when (requireNotNull(currentOperator)) {
+                Operator.ADDITION -> getInputValue1() + percentageOfValue1
+                Operator.SUBTRACTION -> getInputValue1() - percentageOfValue1
+                Operator.MULTIPLICATION -> getInputValue1() * percentageOfValue1
+                Operator.DIVISION -> getInputValue1() / percentageOfValue2
+            }
+            equation.clear().append(ZERO)
+            updateResultOnDisplay(isPercentage = true)
+            inputValue1 = result
+            result = null
+            inputValue2 = null
+            currentOperator = null
+        }
+    }
+
+    private fun onPlusMinusClicked () {
+        if (equation.startsWith(MINUS)) {
+           equation.deleteCharAt(0)
+        } else {
+            equation.insert(0, MINUS)
+        }
+        setInput()
+        updateInputOnDisplay()
+    }
+
+    private fun onAllClearClicked() {
+        inputValue1 = 0.0
+        inputValue2 = null
+        currentOperator = null
+        result = null
+        equation.clear().append(ZERO)
+        clearDisplay()
+    }
+
+    private fun onOperatorClicked(operator: Operator) {
+        onEqualClicked()
+        currentOperator = operator
+    }
+
+    private fun onEqualClicked() {
+        if(inputValue2 != null) {
+            result = calculate()
+            equation.clear().append(ZERO)
+            updateResultOnDisplay()
+            inputValue1 = result
+            result = null
+            inputValue2 = null
+            currentOperator = null
+        } else {
+            equation.clear().append(ZERO)
+        }
+    }
+
+    private fun calculate(): Double {
+        return when (requireNotNull(currentOperator)) {
+            Operator.ADDITION -> getInputValue1() + getInputValue2()
+            Operator.SUBTRACTION -> getInputValue1() - getInputValue2()
+            Operator.MULTIPLICATION -> getInputValue1() * getInputValue2()
+            Operator.DIVISION -> getInputValue1() / getInputValue2()
+        }
+    }
+
+    private fun onDecimalPointClicked() {
+        if (equation.contains(DECIMAL)) return
+        equation.append(DECIMAL)
+        setInput()
+        updateInputOnDisplay()
+    }
+
+    private fun onZeroClicked() {
+        if (equation.startsWith(ZERO)) return
+        onNumberClicked(ZERO)
+    }
+
+    private fun onDoubleZeroClicked() {
+        if (equation.startsWith(ZERO)) return
+        onNumberClicked(DOUBLE_ZERO)
+    }
+
+    private fun getNumericButtons() = with(binding) {
+        arrayOf(
+            buttonOne,
+            buttonTwo,
+            buttonThree,
+            buttonFour,
+            buttonfive,
+            buttonSix,
+            buttonSeven,
+            buttonEight,
+            buttonNine
+        )
+    }
+
+    private fun onNumberClicked(numberText: String) {
+        if(equation.startsWith(ZERO)) {
+            equation.deleteCharAt(0)
+        } else if(equation.startsWith("$MINUS$ZERO")) {
+            equation.deleteCharAt(1)
+        }
+        equation.append(numberText)
+        setInput()
+        updateInputOnDisplay()
+    }
+
+    private fun setInput() {
+        if(currentOperator == null) {
+            inputValue1 = equation.toString().toDouble()
+        } else {
+            inputValue2 = equation.toString().toDouble()
+        }
+    }
+
+    private fun clearDisplay() {
+        with(binding) {
+            textInput.text = getFormatedDisplayValue(value = getInputValue1())
+            textEquation.text = null
+        }
+    }
+
+    private fun updateResultOnDisplay(isPercentage: Boolean = false) {
+        binding.textInput.text = getFormatedDisplayValue(value = result)
+        var input2Text = getFormatedDisplayValue(value = getInputValue2())
+
+        if(isPercentage) input2Text = "$input2Text${getString(R.string.percentage)}"
+        binding.textEquation.text = String.format(
+            "%s %s %s",
+            getFormatedDisplayValue(value = getInputValue1()),
+            getOperatorSymbol(),
+            input2Text
+        )
+    }
+
+    private fun updateInputOnDisplay() {
+        if(result == null) {
+            binding.textEquation.text = null
+        }
+        binding.textInput.text = equation
+    }
+
+    private fun getInputValue1() = inputValue1 ?: 0.0
+    private fun getInputValue2() = inputValue2 ?: 0.0
+
+    private fun getOperatorSymbol(): String {
+        return when (requireNotNull(currentOperator)) {
+            Operator.ADDITION -> getString(R.string.addition)
+            Operator.SUBTRACTION -> getString(R.string.subtraction)
+            Operator.MULTIPLICATION -> getString(R.string.multiplication)
+            Operator.DIVISION -> getString(R.string.division)
+        }
+    }
+
+    private fun getFormatedDisplayValue(value: Double?): String? {
+        val originalValue = value ?: return null
+        return if (originalValue % 1 == 0.0) {
+            originalValue.toInt().toString()
+        } else {
+            originalValue.toString()
+        }
+    }
+
+    enum class Operator {
+        ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION
+    }
+
+    private companion object {
+        const val DECIMAL = "."
+        const val ZERO = "0"
+        const val MINUS = "-"
+        const val DOUBLE_ZERO = "00"
+    }
+}
